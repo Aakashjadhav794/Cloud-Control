00f5d01d7e6d74266c4899164bc8e85301d3a3f0
AAKASH SAMADHAN JADHAV <aakashjadhav792@gmail.com>
2026-01-22 03:24:52 +0530

    update

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
+import API_BASE_URL from "../config/api"
 
 export default function Login() {
   const navigate = useNavigate()
@@ -23,11 +24,12 @@ export default function Login() {
     try {
       setLoading(true)
 
-      const res = await fetch("http://localhost:5000/auth/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(form),
-      })
+const res = await fetch(`${API_BASE_URL}/auth/login`, {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify(form),
+})
+
 
       const data = await res.json()
 
